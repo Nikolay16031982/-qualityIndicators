4e8f76449bd3f757c4a507a867536b6304378de7
Nikolay16031982 <kolya.kuzmichev.82@mail.ru>
2020-10-16 00:45:40 +0300

    Updates

---
+++ precache-manifest.9b0e3d2aca7df522b26abcfb64da3d63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a80db167a9d0b60a6b82a32816ceb8",
+    "url": "/-qualityIndicators/index.html"
+  },
+  {
+    "revision": "a74d7bec321f4167e56f",
+    "url": "/-qualityIndicators/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d6c78445e737ef721ddf",
+    "url": "/-qualityIndicators/static/css/main.531134ab.chunk.css"
+  },
+  {
+    "revision": "a74d7bec321f4167e56f",
+    "url": "/-qualityIndicators/static/js/2.0fa2bd8c.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/-qualityIndicators/static/js/2.0fa2bd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c78445e737ef721ddf",
+    "url": "/-qualityIndicators/static/js/main.6128995f.chunk.js"
+  },
+  {
+    "revision": "15b4a7e1599d7491942c",
+    "url": "/-qualityIndicators/static/js/runtime-main.c90c04b9.js"
+  }
+]);
